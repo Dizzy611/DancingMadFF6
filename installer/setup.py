aae867403fc1e3344d73331c81b26ba09f009931
unknown <dmorrison@SHODAN.local.somebodyelsesproblem.org>
2015-09-28 04:58:33 -0500

    cxfreeze setup.py commit for turning Installer into EXE. Seems to work.

---
+++ installer/setup.py
@@ -0,0 +1,18 @@
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need
+# fine tuning.
+buildOptions = dict(packages = [], excludes = [])
+
+import sys
+base = 'Win32GUI' if sys.platform=='win32' else None
+
+executables = [
+    Executable('Installer.py', base=base)
+]
+
+setup(name='Dancing Mad Installer',
+      version = '15.09.28',
+      description = 'Installer for Dancing Mad FF6 mod, Pre-Alpha.',
+      options = dict(build_exe = buildOptions),
+      executables = executables)
