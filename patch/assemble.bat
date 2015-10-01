4bfa93304d101c7f7e8bff1baf451e453f724288
unknown <dmorrison@SHODAN.local.somebodyelsesproblem.org>
2015-10-01 04:17:12 -0500

    Revert "Fixed the fix. The chosen extended RAM address was not actually unused. $7ef001 however is. This is why I'm not pushing this to alpha testers yet... :P"
    This reverts commit 5090ab0cb702832c1e477f3f5637d727dbc2c422.

--- patch/assemble.bat
+++
@@ -1,4 +0,0 @@
-path C:\Users\dmorrison.LOCAL\Documents\BitBucket\DancingMadNew\utils;C:\Users\dmorrison.LOCAL\Documents\BitBucket\DancingMadNew\utils\wla-dx-9.5-win32-bin-2013-SVN-rev8-WJ;%PATH%
-wla-65816 -vo ff3msu.asm ff3msu.obj
-wlalink -vr ff3msu.link ff3msu.sfc
-flips --create --ips ff3.sfc ff3msu.sfc ff3msu.ips
