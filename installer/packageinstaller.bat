ad2c017b6cd0d783ac837146536e6733d9f1abdf
Dylan Morrison <dmorrison@shodan.local.somebodyelsesproblem.org>
2017-10-06 22:00:45 -0400

    Windows Installer build automation, first draft. Fixes issue #8

--- installer/packageinstaller.bat
+++ installer/packageinstaller.bat
@@ -1,1 +1,17 @@
+@echo off
+python setup.py build
+mkdir temp
+xcopy /E build\exe.win-amd64-3.5 temp
+copy ff3msu.ips temp
+copy ff3msu.msu temp
+copy ff3msu.xml temp
+copy *.html temp 
+copy *.bml temp
+copy *.dat temp
+copy *.csv temp
+cd temp
+"C:\Program Files\7-Zip\7z.exe" a -r DancingMadInstaller.7z *.*
+move DancingMadInstaller.7z ..
+cd ..
 copy /b 7zS.sfx + config.txt + DancingMadInstaller.7z DancingMadInstaller.exe
+echo Assuming no errors, the installer exe should now be built. Remember when moving to Python 3.6 to adjust the build directory.
