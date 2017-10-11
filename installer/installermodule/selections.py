5efb0a1e6654ea99ce902d054c051ea9c56cb153
Dylan Morrison <dmorrison@shodan.local.somebodyelsesproblem.org>
2017-10-11 19:24:04 -0400

    Python-end work for supporting sound effects/misc in installer.

--- installer/installermodule/selections.py
+++ installer/installermodule/selections.py
@@ -1,14 +1,14 @@
 
 # Selections for different presets.
 
-SELECTION_RECOMMENDED = [2]*2 + [1]*2 + [2]*6 + [3] + [1] + [2] + [1] + [3] + [2]*7 + [3]*6 + [0] + [3]*2 + [4]*2 + [0]*3 + [3]*2 + [0] + [3] + [0] + [3]*7 + [0]*3 + [3]*8 # "Sid's Selections"
-SELECTION_OST = [0]*59 # All OST
-SELECTION_FFT = [1]*16 + [0]*43 # 16 FFT tracks and 43 OST tracks
-SELECTION_SSC = [2]*25 + [0]*34 # 25 SSC tracks and 34 OST tracks
-SELECTION_OCR = [3]*21 + [0] + [3]*10 + [0] + [3]*5 + [0] + [3]*20 # 56 OCR tracks, with 3 OST tracks in various missing spots.
+SELECTION_RECOMMENDED = [2]*2 + [1]*2 + [2]*6 + [3] + [1] + [2] + [1] + [3] + [2]*7 + [3]*6 + [0] + [3]*2 + [4]*2 + [0]*3 + [3]*2 + [0] + [3] + [0] + [3]*7 + [0]*3 + [3]*8 + [4] # "Sid's Selections"
+SELECTION_OST = [0]*60 # All OST
+SELECTION_FFT = [1]*16 + [0]*44 # 16 FFT tracks and 43 OST tracks
+SELECTION_SSC = [2]*25 + [0]*35 # 25 SSC tracks and 34 OST tracks
+SELECTION_OCR = [3]*21 + [0] + [3]*10 + [0] + [3]*5 + [0] + [3]*20 + [4] # 56 OCR tracks, with 3 OST tracks in various missing spots.
 SELECTION_OCR[31] = 4 # Replace OCR opera/finale with OTH 
 SELECTION_OCR[32] = 4 #
-SELECTION_OCRALT = [5]*21 + [0] + [5]*10 + [0] + [5]*5 + [0] + [5]*20 # 56 OCR tracks, alternate loops, with 3 OST tracks in various missing spots.
+SELECTION_OCRALT = [5]*21 + [0] + [5]*10 + [0] + [5]*5 + [0] + [5]*20 + [4] # 56 OCR tracks, alternate loops, with 3 OST tracks in various missing spots.
 SELECTION_OCRALT[31] = 4 # Replace OCR opera/finale with OTH 
 SELECTION_OCRALT[32] = 4 #
 
