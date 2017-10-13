48b13df567d88295bcc98ddc5193cfc748ea67dd
Dylan Morrison <dmorrison@shodan.local.somebodyelsesproblem.org>
2017-10-12 20:40:00 -0400

    Make selections.py easier to maintain.
     - Expands Recommended selection array for easier changes.
     - Comments added to each entry of Recommended selection showing
       what track the entry is for.

--- installer/installermodule/selections.py
+++ installer/installermodule/selections.py
@@ -1,14 +1,72 @@
 
 # Selections for different presets.
 
-SELECTION_RECOMMENDED = [2]*2 + [1]*2 + [2]*6 + [3] + [1] + [2] + [1] + [3] + [2]*7 + [3]*6 + [0] + [3]*2 + [4]*2 + [0]*3 + [3]*2 + [0] + [3] + [0] + [3]*7 + [0]*3 + [3]*8 + [4] # "Sid's Selections"
+# 0 = OST, 1 = FFT, 2 = SSC, 3 = OCR, 4 = OTH, 5 = OCR2, 6 = None (only used in download page)
+
+SELECTION_RECOMMENDED =  [2, # 1-01 Omen
+                          2, # 1-02 The Mines of Narshe
+                          1, # 1-03 Awakening
+                          1, # 1-04 Locke's Theme
+                          2, # 1-05 Battle Theme
+                          2, # 1-06 Fanfare
+                          2, # 1-07 Edgar & Sabin
+                          2, # 1-08 Kefka
+                          2, # 1-09 Mt Koltz
+                          2, # 1-10 The Returners
+                          3, # 1-11 Shadow's Theme
+                          1, # 1-12 Troops March On
+                          2, # 1-13 Cyan's Theme
+                          1, # 1-14 The Unforgiven
+                          3, # 1-15 The Phantom Forest
+                          2, # 1-16 The Phantom Train
+                          2, # 1-17 The Veldt
+                          2, # 1-18 Gau's Theme
+                          2, # 1-19 The Serpent Trench
+                          2, # 1-20 Kids Run Through the City
+                          2, # 1-21 Under Martial Law
+                          2, # 1-22 Celes
+                          3, # 1-23 Protect the Espers!
+                          3, # 1-24 The Decisive Battle
+                          3, # 1-25 Metamorphosis
+                          3, # 2-01 Terra's Theme
+                          3, # 2-02 Coin of Fate
+                          3, # 2-03 Techno de Chocobo
+                          0, # 2-04 Forever Rachel
+                          3, # 2-05 Slam Shuffle
+                          3, # 2-06 Spinach Rag
+                          4, # 2-07/2-08/2-09 The Opera 
+                          4, # 2-10 Grand Finale?
+                          0, # 2-11 Setzer's Theme
+                          0, # 2-12 Johnny C. Bad
+                          0, # 2-13 The Gestahl Empire
+                          3, # 2-14 Magitek Research Facility [3]*2 + [0] + [3] + [0] + [3]*7 + [0]*3 + [3]*8 + [4]
+                          3, # 2-15 The Airship Blackjack
+                          0, # 2-16 What??
+                          3, # 2-17 Mog's Theme
+                          0, # 2-18 Strago's Theme
+                          3, # 2-19 Relm's Theme
+                          3, # 2-20 Esper World
+                          3, # 3-01 Floating Continent
+                          3, # 3-02 Catastrophe
+                          3, # 3-03 Battle to the Death
+                          3, # 3-04 Rest in Peace
+                          3, # 3-05 Dark World
+                          0, # 3-06 From That Day On...
+                          0, # 3-07 Searching for Friends
+                          0, # 3-08 Gogo's Theme
+                          3, # 3-09 Epitaph
+                          3, # 3-10 The Magic House
+                          3, # 3-11 Umaro's Theme
+                          3, # 3-12 The Fanatics
+                          3, # 3-13 Kefka's Tower
+                          4, # 3-14 Dancing Mad
+                          3, # 3-15 Ending Theme
+                          3, # 3-16 The Prelude
+                          4] # Sound Effects and Miscellany
+                          
 SELECTION_OST = [0]*60 # All OST
 SELECTION_FFT = [1]*16 + [0]*44 # 16 FFT tracks and 43 OST tracks
 SELECTION_SSC = [2]*25 + [0]*35 # 25 SSC tracks and 34 OST tracks
-SELECTION_OCR = [3]*21 + [0] + [3]*10 + [0] + [3]*5 + [0] + [3]*20 + [4] # 56 OCR tracks, with 3 OST tracks in various missing spots.
-SELECTION_OCR[31] = 4 # Replace OCR opera/finale with OTH 
-SELECTION_OCR[32] = 4 #
-SELECTION_OCRALT = [5]*21 + [0] + [5]*10 + [0] + [5]*5 + [0] + [5]*20 + [4] # 56 OCR tracks, alternate loops, with 3 OST tracks in various missing spots.
-SELECTION_OCRALT[31] = 4 # Replace OCR opera/finale with OTH 
-SELECTION_OCRALT[32] = 4 #
+SELECTION_OCR = [3]*21 + [0] + [3]*9 + [4]*2 + [3]*5 + [0] + [3]*20 + [4] # 56 OCR tracks, with 3 OST tracks in various missing spots.
+SELECTION_OCRALT = [5]*21 + [0] + [5]*9 + [4]*2 + [5]*5 + [0] + [5]*20 + [4] # 56 OCR tracks, alternate loops, with 3 OST tracks in various missing spots.
 
--- installer/packageinstaller.bat
+++ installer/packageinstaller.bat
@@ -1,26 +1,29 @@
 @echo off
 python setup.py build
-mkdir temp
-xcopy /E /Y /Q build\exe.win-amd64-3.6 temp
-copy /Y ff3msu.ips temp
-copy /Y ff3msu.msu temp
-copy /Y ff3msu.xml temp
-copy /Y *.html temp 
-copy /Y *.bml temp
-copy /Y *.dat temp
-copy /Y *.csv temp
-copy /Y *.ico temp
-copy /Y *.png temp
+mkdir tmp
+xcopy /E /Y /Q build\exe.win-amd64-3.6 tmp
+rem Update IPS if a new one has been built but not copied into new dir.
+fc /b ff3msu.ips ..\patch\ff3msu.ips
+if errorlevel 1 copy /Y ..\patch\ff3msu.ips ff3msu.ips
+copy /Y ff3msu.ips tmp
+copy /Y ff3msu.msu tmp
+copy /Y ff3msu.xml tmp
+copy /Y *.html tmp 
+copy /Y *.bml tmp
+copy /Y *.dat tmp
+copy /Y *.csv tmp
+copy /Y *.ico tmp
+copy /Y *.png tmp
 rem Remove pyqt5 bloat. Every DLL and such in the following dir that's *actually used* is copied into the root folder anyway.
-rmdir /S /Q temp\PyQt5\Qt
-cd temp
+rmdir /S /Q tmp\PyQt5\Qt
+cd tmp
 "C:\Program Files\7-Zip\7z.exe" a -r DancingMadInstaller.7z *.*
 move DancingMadInstaller.7z ..
 cd ..
 copy /b 7zS.sfx + config.txt + DancingMadInstaller.7z DancingMadInstaller.exe
-..\utils\reshacker\ResourceHacker.exe -open DancingMadInstaller.exe -save DancingMadInstaller-temp.exe -action addoverwrite -res kefka.ico -mask ICONGROUP,MAINICON,
-move /Y DancingMadInstaller-temp.exe DancingMadInstaller.exe
-rmdir /S /Q temp
+..\utils\reshacker\ResourceHacker.exe -open DancingMadInstaller.exe -save DancingMadInstaller-tmp.exe -action addoverwrite -res kefka.ico -mask ICONGROUP,MAINICON,
+move /Y DancingMadInstaller-tmp.exe DancingMadInstaller.exe
+rmdir /S /Q tmp
 rmdir /S /Q build
 del DancingMadInstaller.7z
 echo Assuming no errors, the installer exe should now be built.
