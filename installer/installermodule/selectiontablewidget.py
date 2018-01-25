f2790b68b3f1f4ca05db9fd4aa50a1dccebd4665
Dylan Morrison <dylanjmorrison611@gmail.com>
2018-01-24 23:28:22 -0500

    Installer Hotfix One Rollup. (#63)
    - Changed installer to use XML files instead of hand-rolled data formats.
    - Changed installer to use a mirror list instead of hard-coded mirrors, added Amazon Web Services mirror.
    - Added ChrystalChameleon and Final Fantasy Acoustic Rendition to our music sources.
    - Updated Developer's Recommendation for new sources.
    - Added Rodimus's Ted Woolsey Uncensored Edition automatic IPS download and patching support. (Pending permission to distribute)
    - Added Madsiur's Music Player Patch, Dancing Mad Edition automatic IPS download and patching support.
    - Added edale2's Cut Songs Restoration Patch, automatic IPS and PCM download and patching support.
    - Various minor bugfixes.
    - Documentation on how to trace routines in a ROM. Originally intended for edale2, made public for others to come across.

--- installer/installermodule/selectiontablewidget.py
+++ installer/installermodule/selectiontablewidget.py
@@ -2,25 +2,21 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 import csv
-from installermodule.selections import *
-
-sources = [ "OST", "FFT", "SST", "OCR", "OTH", "OCR2", "SPC" ]
+from installermodule import song
 
 class selectionTableWidget(QtWidgets.QTableWidget):
         def __init__(self, parent):
+                self.spccol = 0
                 super().__init__(parent)
-
+                self.songlist = song.parseSongXML("songs.xml")
+                
                 self.setGeometry(QtCore.QRect(0, 10, 551, 401))
                 self.setCornerButtonEnabled(False)
                 self.setObjectName("tableWidget")
                 
-                # Grab number of rows/columns based on file entries, set up headers.
-                with open("trackSources.csv") as f:
-                    tmp = csv.reader(f)
-                    colcount = len(next(tmp))
+                colcount = len(self.songlist.sources)
+                rowcount = len(self.songlist.songs)
                 self.setColumnCount(colcount)
-                with open("trackTitles.dat") as f:
-                    rowcount = sum((1 for _ in f))
                 self.setRowCount(rowcount)
                 for i in range(0, rowcount):
                     item = QtWidgets.QTableWidgetItem()
@@ -35,77 +31,68 @@ class selectionTableWidget(QtWidgets.QTableWidget):
                 self.verticalHeader().setDefaultSectionSize(20)
                 self.verticalHeader().setMinimumSectionSize(10)
                 self.verticalHeader().setStretchLastSection(False)
-                for idx,source in enumerate(sources):
+                for idx,source in enumerate(self.songlist.sources):
                     item = self.horizontalHeaderItem(idx)
                     print("DEBUG. Source header:", source)
-                    item.setText(source)
+                    item.setText(source.upper())
                     item.setTextAlignment(QtCore.Qt.AlignCenter)
 
-                # Initialize song list and fill with 0s.
-                self.songList = SELECTION_OST
+                # Initialize selection list and fill with 0s.
+                self.selections = [0]*len(self.songlist.songs)
 
-                # Read list of track titles and place as vertical header text.
-                index = 0
-                with open("trackTitles.dat") as f:
-                        for idx, line in enumerate(f):
-                            item = self.verticalHeaderItem(idx)
-                            print("DEBUG: Song header:", line)
-                            item.setText(line)
-                            item.setTextAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignLeft)
-                               
+                for idx,sng in enumerate(self.songlist.songs):
+                    item = self.verticalHeaderItem(idx)
+                    print("DEBUG: Song header:", sng.name)
+                    item.setText(sng.name)
+                    item.setTextAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignLeft)
 
                 # Read valid sources csv and populate radio buttons based on this.
                 self.myButtons = []
-                with open("trackSources.csv") as csvfile:
-                        csvreader = csv.reader(csvfile)
-                        rowindex = 0
-                        for song in csvreader:
-                                selectspc = False
-                                self.myButtons.append([])
-                                colindex = 0
-                                radios = []
-                                radioGroup = QtWidgets.QButtonGroup(self)
-                                for source in song:
-                                        if colindex == 0 and source == "0":
-                                            selectspc = True
-                                        if source == "1":
-                                                radios.append(QtWidgets.QRadioButton())
-                                                radios[-1].myRowCol = (rowindex, colindex) # Dirty, dirty hack. I feel unclean... And it is glorious. Thank you, Python Gods.
-                                                radios[-1].clicked.connect(self.buttonClicked)
-                                                if colindex == 0:
-                                                      radios[-1].setChecked(True)
-                                                if colindex == 6 and selectspc == True:
-                                                      radios[-1].setChecked(True)
-                                                self.setCellWidget(rowindex, colindex, radios[-1])
-                                                radioGroup.addButton(radios[-1])
-                                                self.myButtons[rowindex].append(radios[-1])
-                                        else:
-                                                item = QtWidgets.QTableWidgetItem()
-                                                item.setFlags(QtCore.Qt.NoItemFlags)
-                                                self.setItem(rowindex, colindex, item)
-                                                self.myButtons[rowindex].append(None)
-                                        colindex = colindex + 1
-                                rowindex = rowindex + 1
+                for row,sng in enumerate(self.songlist.songs):
+                    self.myButtons.append([])
+                    radios = []
+                    radioGroup = QtWidgets.QButtonGroup(self)
+                    selectspc = not(sng.sourceCheck("ost"))
+                    for col,source in enumerate(self.songlist.sources):
+                        if source == "spc":
+                            self.spccol = col
+                        if sng.sourceCheck(source) == True:
+                            radios.append(QtWidgets.QRadioButton())
+                            radios[-1].myRowCol = (row, col)
+                            radios[-1].clicked.connect(self.buttonClicked)
+                            if col == 0 or (source == "spc" and selectspc == True):
+                                radios[-1].setChecked(True)
+                            self.setCellWidget(row, col, radios[-1])
+                            radioGroup.addButton(radios[-1])
+                            self.myButtons[row].append(radios[-1])
+                        else:
+                            item = QtWidgets.QTableWidgetItem()
+                            item.setFlags(QtCore.Qt.NoItemFlags)
+                            self.setItem(row, col, item)
+                            self.myButtons[row].append(None)
 
         
         @QtCore.pyqtProperty(QtCore.QVariant)
         def SongList(self):
-                return QtCore.QVariant(self.songList)
+                return QtCore.QVariant(self.selections)
 
         def buttonClicked(self):
                 button = self.sender()
                 songNum = button.myRowCol[0]
                 source = button.myRowCol[1]
-                self.songList[songNum] = source
+                self.selections[songNum] = source
         
         def reloadSources(self, sources):
-            self.songList = sources
-            for song,source in enumerate(self.songList):
+            self.selections = sources
+            for sng,source in enumerate(self.selections):
                 try:
-                    if self.myButtons[song][source] is not None:
-                        self.myButtons[song][source].setChecked(True)
+                    if self.myButtons[sng][source] is not None:
+                        self.myButtons[sng][source].setChecked(True)
                     else:
-                        self.myButtons[song][0].setChecked(True)
-                        print("DEBUG: Asked to load a source that's not available. Defaulting to OST.")
+                        if self.myButtons[sng][0] is not None:
+                            self.myButtons[sng][0].setChecked(True)
+                        else:
+                            self.myButtons[sng][self.spccol].setChecked(True)
+                        print("DEBUG: Asked to load a source that's not available. Defaulting to OST or SPC.")
                 except TypeError as e:
-                    print("DEBUG: TypeError:", song, source, str(e))
+                    print("DEBUG: TypeError:", sng, source, str(e))
