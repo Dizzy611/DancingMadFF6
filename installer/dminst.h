2f398f7f31864efd75392f0cb1cfbf0b925493d0
Dylan Morrison <dizzy@domad.science>
2024-09-16 02:10:39 -0400

    DanceMonkey semi-working prototype with working ROM validator.

---
+++ installer/dminst.h
@@ -0,0 +1,28 @@
+#ifndef DMINST_H
+#define DMINST_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class DMInst;
+}
+QT_END_NAMESPACE
+
+class DMInst : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    DMInst(QWidget *parent = nullptr);
+    ~DMInst();
+
+private slots:
+    void on_ROMSelectBrowse_clicked();
+
+    void on_ROMSelectLine_textChanged(const QString &arg1);
+
+private:
+    Ui::DMInst *ui;
+};
+#endif // DMINST_H
