1b2b7ef3c4fdc32b288b8bb754344d60539cf986
Dylan Morrison <dizzy@domad.science>
2024-09-17 21:18:39 -0400

    songs.xml parser almost complete, song parsing finished.

--- installer/song_parser.cpp
+++ installer/song_parser.cpp
@@ -9,6 +9,12 @@ struct Preset {
     std::map<std::string, std::vector<int>> selections;
 };
 
+struct Song {
+    std::string name;
+    std::vector<int> pcms;
+    std::vector<std::string> sources;
+};
+
 std::pair<int, int> parseRange(std::string input) {
     std::pair<int, int> retval;
     int dash = input.find('-');
@@ -29,6 +35,7 @@ void parseSongsXML(const QString &filename) {
     QDomDocument songxml;
     std::vector<std::string> sources;
     std::vector<struct Preset> presets;
+    std::vector<struct Song> songs;
 
     QFile file(filename);
     if (!file.open(QIODevice::ReadOnly) || !songxml.setContent(&file))
@@ -95,6 +102,78 @@ void parseSongsXML(const QString &filename) {
         }
     }
 
+    // Get and parse all songs
+    QDomNodeList songs_xml = songxml.elementsByTagName("song");
+    for (int i = 0; i < songs_xml.size(); i++) {
+        struct Song song;
+        QDomNode root = songs_xml.item(i);
+
+        // Find name
+        QDomElement name_xml = root.firstChildElement("name");
+        std::string name = name_xml.text().toStdString();
+        song.name = name;
+
+        // Build vector of PCMs
+        QDomElement pcms_xml = root.firstChildElement("pcms");
+        std::string rawpcms = pcms_xml.text().toStdString();
+        std::vector<int> pcms;
+        // Check if multiple PCMs or just one
+        if (rawpcms.find(',') != std::string::npos) {
+            // If multiple entries, pull out each one and add it to the vector
+            std::stringstream pcmss(rawpcms);
+            std::string token;
+            while (std::getline(pcmss, token, ',')) {
+                pcms.push_back(std::stoi(token));
+            }
+        } else {
+            // Otherwise just add it as is
+            pcms.push_back(std::stoi(rawpcms));
+        }
+        song.pcms = pcms;
+
+        // Build vector of sources
+        std::vector<std::string> sources;
+        QDomNodeList song_nodes = root.childNodes();
+        for (int j = 0; j < song_nodes.size(); j++) {
+            if (song_nodes.item(j).nodeName() == "name" || song_nodes.item(j).nodeName() == "pcms" || song_nodes.item(j).nodeName() == "preset") {
+                continue;
+            } else {
+                sources.push_back(song_nodes.item(j).nodeName().toStdString());
+            }
+        }
+        song.sources = sources;
+
+        // Populate per song presets
+        QDomElement preset = root.firstChildElement("preset");
+        for (; !preset.isNull(); preset = preset.nextSiblingElement("preset")) {
+            if (preset.hasAttribute("name")) {
+                // Find preset with the given name in previously populated vector
+                for (auto & element : presets) {
+                    if (element.name == preset.attribute("name").toStdString()) {
+                        // Find source being set for preset. Should be name of very first child node
+                        std::string source = preset.childNodes().item(0).nodeName().toStdString();
+                        // Check if source already exists in preset
+                        if (element.selections.find(source) == element.selections.end()) {
+                            // Source does not already exist in preset, add it.
+                            std::vector<int> ranges;
+                            ranges.push_back(i);
+                            ranges.push_back(i);
+                            element.selections.insert({source, ranges});
+                        } else {
+                            // Source exists in preset, append to it
+                            element.selections[source].push_back(i);
+                            element.selections[source].push_back(i);
+                        }
+                    }
+                }
+            }
+        }
+
+        // Add song to vector
+        songs.push_back(song);
+
+    }
+
     // DEBUG
     std::cout << "PRESET LIST:" << std::endl;
     for (auto & element : presets) {
@@ -109,4 +188,19 @@ void parseSongsXML(const QString &filename) {
         }
     }
 
+    // DEBUG
+    std::cout << "SONG LIST:" << std::endl;
+    for (auto & element : songs) {
+        std::cout << "\t" << element.name << std::endl;
+        std::cout << "\t\tPCMs:";
+        for (auto & element2 : element.pcms) {
+            std::cout << element2 << ",";
+        }
+        std::cout << std::endl;
+        std::cout << "\t\tSources:";
+        for (auto & element2 : element.sources) {
+            std::cout << element2 << ",";
+        }
+        std::cout << std::endl;
+    }
 }
